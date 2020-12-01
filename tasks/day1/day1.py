c9dc1c37530ede3c93155db9ca4f2f9994735536
TTumyr <trond.tumyr@online.no>
2020-12-01 19:25:41 +0100

    day 1

---
+++ tasks/day1/day1.py
@@ -0,0 +1,37 @@
+from data import number_txt_string
+
+def day1():
+    def string_split(numstr):
+        return numstr.split()
+
+    def convert_to_number(numstr):
+        numbers = []
+        for entry in numstr:
+            numbers.append(int(entry))
+        return numbers
+
+    def return_numbers(numbers):
+        two_numbers = []
+        three_numbers = []
+        for i, num in enumerate(numbers, start=0):
+            for j, num2 in enumerate(numbers, start=i+1):
+                if num + num2 == 2020:
+                    two_numbers = [num, num2]
+                for k, num3 in enumerate(numbers, start=j+1):
+                    if num + num2 + num3 == 2020:
+                        three_numbers = [num, num2, num3]
+        return two_numbers, three_numbers
+    def calculate_answer(numbers):
+        print(numbers)
+        answer_two_num = numbers[0][0] * numbers[0][1]
+        answer_three_num = numbers[1][0] * numbers[1][1] * numbers[1][2]
+        return numbers, answer_two_num, answer_three_num
+
+    def init():
+        numbers = calculate_answer(return_numbers(convert_to_number(string_split(number_txt_string))))
+        print("The relevant numbers -- ", numbers[0])
+        print("Solution -- two numbers multiplication: ", numbers[1])
+        print("Solution -- three numbers multiplication: ", numbers[2])
+    init()
+
+day1()
